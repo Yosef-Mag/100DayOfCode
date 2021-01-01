fcad5d0a3b9ae02f31fe925ca45369d9deb0ac44
Yosef-Mag <yosef.maghaireh@gmail.com>
2021-01-01 19:00:00 +0200

    miising day1

---
+++ missing days/week1 day1/sumPrimes.js
@@ -0,0 +1,25 @@
+// write a function that sums all the prime numbers
+// below what you pass to it. example:
+// sumPrimes(10) // => 17
+
+function sumPrimes(num) {
+    function isPrime(number) {
+      for (let current = number - 1; current > 1; current--) {
+        if (number % current === 0) {
+          return false
+        }
+      }
+      return true
+    }
+  
+    const arr = []
+    const counter = 0
+    for (let i = num; i > 1; i--) {
+      if (isPrime(i)) {
+        arr.push(i)
+      }
+    }
+    return arr.reduce(function (a, b) {
+      return a + b
+    })
+  }
